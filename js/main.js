991796869768f8768fec11c9a1df0ab1ec1a2493
kyoro <hirohisaonuma@icloud.com>
2020-09-29 02:12:44 +0900

    Omikugi

--- js/main.js
+++ js/main.js
@@ -2,19 +2,61 @@
 
 {
   const btn = document.getElementById("btn");
-
   btn.addEventListener("click", () => {
-    // const results = ["大吉", "中吉", "凶", "末吉"];
-    // const results = ["大吉", "大吉", "大吉", "大吉", "凶"];
-    // btn.textContent = results[Math.floor(Math.random() * results.length)];
+    const results = [
+      "天才!!",
+      "凡人。",
+      "凡人。",
+      "凡人。",
+      "凡人。",
+      "凡人。",
+      "凡人。",
+      "只者…",
+      "只者…",
+      "只者…",
+      "一般人",
+      "一般人",
+      "一般人",
+      "一般人",
+      "一般人",
+      "並みの者",
+      "並みの者",
+      "並みの者",
+      "並みの者",
+      "フツーの人",
+      "フツーの人",
+      "フツーの人",
+      "フツーの人",
+      "フツーの人",
+      "フツーの人",
+      "フツーの人",
+      "標準的な人",
+      "標準的な人",
+      "標準的な人",
+      "パンピー",
+      "パンピー",
+      "市民",
+      "市民",
+    ];
+    btn.textContent = results[Math.floor(Math.random() * results.length)];
 
-    const n = Math.random();
-    if (n < 0.05) {
-      btn.textContent = "大吉";
-    } else if (n < 0.2) {
-      btn.textContent = "中吉";
-    } else {
-      btn.textContent = "凶";
-    }
+    // const n = Math.random();
+    // if (n < 0.05) {
+    //   btn.textContent = "天才!!";
+    // } else if (n < 0.1) {
+    //   btn.textContent = "市民";
+    // } else if (n < 0.1) {
+    //   btn.textContent = "凡人。";
+    // } else if (n < 0.15) {
+    //   btn.textContent = "フツーの人";
+    // } else if (n < 0.15) {
+    //   btn.textContent = "パンピー";
+    // } else if (n < 0.15) {
+    //   btn.textContent = "一般人";
+    // } else if (n < 0.15) {
+    //   btn.textContent = "標準的な人";
+    // } else {
+    //   btn.textContent = "只者…";
+    // }
   });
 }
